48232ed3e45aa55bba507ffa778db67792ad69c0
Marc San Diego <74586578+marcsandiego@users.noreply.github.com>
2021-09-29 14:59:56 +0800

    with change password
    need to edit the UI of edit profile

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ import mysql.connector as mc
 #---Import that will load the UI file---#
 from PyQt5.uic import loadUi
 import detechRs_rc
+import datetime
+from threading import Timer
+import schedule
+import time
 
-
+#link kung saan sinundan ko yung getting info from diff window
+#https://www.youtube.com/watch?v=NrijKenny3Y
 
 #--CLASS CREATED THAT WILL LOAD THE UI FILE
 class Login(QMainWindow):
@@ -22,29 +27,47 @@ class Login(QMainWindow):
         self.loginButton.clicked.connect(self.loginFunction)
         self.registerButton.clicked.connect(self.registerUi)
 
+        #ewan ko ginawa neto parang naging variable lang ni mainPage
+        self.mainWindow = mainPage()
+
         # -- Created a function called "loginFunction" --#
-
     def loginFunction(self):
-        lgUserLine = self.lgUserLine.text()  # -- Getting the textbox context lgUserline --#
+        lgUserLine = self.lgUserLine.text().capitalize()  # -- Getting the textbox context lgUserline --#
         lgPassLine = self.lgPassLine.text()  # -- Getting the textbox context lgPassline --#
+        count = 0
 
-        mydb = mc.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="detech"
-        )
+        if lgUserLine == "":
+            self.UsernameLabelResult.setText("Username cannot be blank!")
+        else:
+            self.UsernameLabelResult.setText("")
+            count += 1
 
-        mycursor = mydb.cursor()
-        query = "SELECT * FROM users WHERE '" + lgUserLine + "' LIKE username AND '" + lgPassLine + "' LIKE password"
-        mycursor.execute(query)
-        result = mycursor.fetchone()
+        if lgPassLine == "":
+            self.PasswordLabelResult.setText("Password cannot be blank!")
+        else:
+            self.PasswordLabelResult.setText("")
+            count += 1
 
-        if result is None:
-            self.labelResult.setText("Incorrect username and/or password. Please try again.")
+        if count == 2:
+            mydb = mc.connect(
+                host="localhost",
+                user="root",
+                password="",
+                database="detech"
+            )
+            mycursor = mydb.cursor()
+            query = "SELECT * FROM users WHERE '" + lgUserLine + "' LIKE username AND '" + lgPassLine + "' LIKE password"
+            mycursor.execute(query)
+            result = mycursor.fetchone()
 
-        else: #call mainPage function
-            self.mainPage()
+            if result is None:
+                self.UsernameLabelResult.setText("Incorrect username and/or password. Please try again.")
+                self.PasswordLabelResult.setText("Incorrect username and/or password. Please try again.")
+                self.lgUserLine.setText("")
+                self.lgPassLine.setText("")
+
+            else: #call mainPage function
+                self.passingInformation()
 
 
     def registerUi(self):
@@ -62,7 +85,7 @@ class Login(QMainWindow):
             password="",
             database="detech"
         )
-        nameRegLine = self.nameRegLine.text()  # -- Getting the textbox context lgUserline --#
+        nameRegLine = self.nameRegLine.text().capitalize()  # -- Getting the textbox context lgUserline --#
         usernameRegLine = self.usernameRegLine.text()  # -- Getting the textbox context lgPassline --#
         emailRegLine = self.emailRegLine.text()  # -- Getting the textbox context lgPassline --#
         passwordRegLine = self.passwordRegLine.text()  # -- Getting the textbox context lgPassline --#
@@ -74,7 +97,7 @@ class Login(QMainWindow):
             self.labelNameError.setText("Name cannot be blank!")
         else:
             self.labelNameError.setText("")
-            count = count + 1
+            count += 1
 
         # goods, check if unique
         if usernameRegLine == "":
@@ -89,10 +112,10 @@ class Login(QMainWindow):
 
             #if unique username, save
             if result is not None:
-                self.labelUsernameError.setText("Username is already in used!")
+                self.labelUsernameError.setText("Username is already in use!")
                 self.usernameRegLine.setText("")
             else:
-                count = count + 1
+                count += 1
 
         # goods - email validator and check if unique
         if emailRegLine == "":
@@ -114,7 +137,7 @@ class Login(QMainWindow):
                     self.emailRegLine.setText("")
                 # if unique email, save
                 else:
-                    count = count + 1
+                    count += 1
 
         # goods, 8 or more character with number
         if passwordRegLine == "":
@@ -128,7 +151,7 @@ class Login(QMainWindow):
                 if str.isalpha(passwordRegLine):
                     self.labelPasswordError.setText("Password must have atleast one integer!")
                 else:
-                    count = count + 1
+                    count += 1
 
         #goods, confirm password
         if confirmPasswordRegLine == "":
@@ -146,17 +169,20 @@ class Login(QMainWindow):
             mycursor = mydb.cursor()
             insert = "INSERT INTO users (name, username, email, password, confirm_password) VALUES (%s, %s, %s, %s, %s)"
             value = (nameRegLine, usernameRegLine, emailRegLine, passwordRegLine, confirmPasswordRegLine)
-
-
             mycursor.execute(insert, value)
             mydb.commit()
             print(" Registration\n", nameRegLine, "\n", usernameRegLine, "\n", emailRegLine, "\n",passwordRegLine, "\n", confirmPasswordRegLine, "\n")
-            # go to registerUI function
-            self.registerUi()
+            # go to login function - should call the class woo
 
 
-    #--FUNCTION FOR MAIN PAGE AFTER LOGGING IN
-    def mainPage(self):
+    def passingInformation(self):
+        self.mainWindow.userDisplayLabel.setText(self.lgUserLine.text())
+        self.mainWindow.displayingInformation()
+        self.mainWindow.displayProfile()
+
+class mainPage(QMainWindow):
+    def __init__(self):
+        super(mainPage, self).__init__()
         # --- FROM THE IMPORT PYQT5.UIC IMPORT LOADUI---##
         loadUi("mainPage_UI.ui",self)
         self.setFixedWidth(1596)
@@ -164,6 +190,11 @@ class Login(QMainWindow):
 
         # --- SET THE PROFILE PAGE AS A DEFAULT PAGE AFTER LOGGING IN --- #
         self.stackedWidget.setCurrentWidget(self.profile_page)
+
+        # date and time = need to improve the time, should be running
+        dateTime = datetime.datetime.now()
+        self.dateDisplay_label.setText('%s/%s/%s' % (dateTime.month, dateTime.day, dateTime.year))
+        self.timeDisplay_label.setText('%s:%s:%s' % (dateTime.hour, dateTime.minute, dateTime.second))
 
         # --- CODED BUTTONS IN ABLE TO SELECT A CERTAIN STACKED WIDGET PAGE --- #
         self.profileButton.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.profile_page)) # Profile button to Profile page
@@ -175,9 +206,12 @@ class Login(QMainWindow):
         self.editProfile_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.editProfile_page)) # Edit profile button to edit profile page
         self.editProfileCancel_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.profile_page)) # -- CANCEL BUTTON --#
 
+
+
+
         #--- CHANGE PASSWORD EDIT PAGE --#
-        self.changePass_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.changePass_page)) #change password button to change password page
-        self.changePassCancel_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.profile_page)) #-- CANCEL BUTTON --#
+        #self.changePass_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.changePass_page)) #change password button to change password page
+        #self.changePassCancel_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.profile_page)) #-- CANCEL BUTTON --#
 
         #--- SETTINGS PAGE WIDGETS ---#
         self.detectionSetup_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.detectionSetup_page)) #detection setup button from settings to detection setup page
@@ -185,6 +219,7 @@ class Login(QMainWindow):
 
         #--- DETECTION SETUP PAGE BUTTONS ---#
         self.dtSetupBack_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.settings_page)) #-- BACK BUTTON DETECTION SETUP, RETURNS TO SETTINGS PAGE ---#
+
         # --- SET THE PROFILE PAGE AS A DEFAULT PAGE AFTER LOGGING IN --- #
         self.setupCCTV_widget.setCurrentWidget(self.cctvAvailableConnect_page)
 
@@ -193,6 +228,7 @@ class Login(QMainWindow):
         self.connectIP_button.clicked.connect(lambda: self.setupCCTV_widget.setCurrentWidget(self.connectIP_page)) #connect IP button to connect IP page
         self.continueConnectIP_button.clicked.connect(lambda: self.setupCCTV_widget.setCurrentWidget(self.cctvConnectedView_page)) # continue to connect ip to cinnection view surveillance sucesss
         self.checkSurveillance_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.surveillance_page)) #check surveillance button redirect to surveillance.
+
         # --- SET THE "ALL CAMERA" PAGE AS A DEFAULT PAGE WHEN SELECTING THE SURVEILLANCE PAGE--- #
         self.surveillance_frame.setCurrentWidget(self.allCamera_page)
 
@@ -202,6 +238,156 @@ class Login(QMainWindow):
         self.camera3_button.clicked.connect(lambda: self.surveillance_frame.setCurrentWidget(self.camera3_page))
         self.camera4_button.clicked.connect(lambda: self.surveillance_frame.setCurrentWidget(self.camera4_page))
         self.allCamera_button.clicked.connect(lambda: self.surveillance_frame.setCurrentWidget(self.allCamera_page))
+
+        self.displayProfile()
+
+
+    def displayingInformation(self):
+        self.show()
+
+    def displayProfile(self):
+        # Display profile
+        username1 = self.userDisplayLabel.text()
+
+        mydb = mc.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="detech"
+        )
+
+        mycursor = mydb.cursor()
+        query = "SELECT * FROM users WHERE '" + username1 + "' LIKE username"
+        mycursor.execute(query)
+        result = mycursor.fetchall()
+
+        for row in result:
+            self.nameDisplay_label.setText(row[0])
+            self.nameStore_label.setText(row[5])
+            self.typeStore_label.setText(row[6])
+            self.storeAddDisplay_label.setText(row[7])
+            self.cityDisplay_label.setText(row[8])
+            self.countryDisplay_label.setText(row[9])
+
+        self.savePass_button.clicked.connect(self.changePassword)
+        self.changePass_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.changePass_page))  # change password button to change password page
+        self.saveChanges_button.clicked.connect(self.editProfile)
+
+
+    '''
+    #edit profile = recreate like change password
+    def editProfile(self):
+        username1 = self.userDisplayLabel.text()
+        editUsername = self.editUsername.text()
+        #editOwner = self.editOwner.text()
+        #editStore = self.editStore.text()
+        #editType = self.editType.text()
+        #editAddress = self.editAddress.text()
+        #editCity = self.editCity.text()
+        #EditCountry = self.EditCountry.text()
+        count = 0
+
+        mydb = mc.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="detech"
+        )
+
+        if editUsername == "":
+            mycursor = mydb.cursor()
+            query = "SELECT * FROM users WHERE '" + username1 + "' LIKE username"
+            mycursor.execute(query)
+            result = mycursor.fetchall()
+
+            for row in result:
+                self.editUsername.setText(row[1])
+                count += 1
+            print(editUsername)
+
+        else:
+            mycursor = mydb.cursor()
+            query = "SELECT * FROM users WHERE '" + editUsername + "' LIKE username"
+            mycursor.execute(query)
+            result = mycursor.fetchone()
+
+            if result is not None:
+                self.editUsernameLabel.setText("Username is already in use!")
+            else:
+                self.userDisplayLabel.setText(editUsername)
+                count += 1
+
+        if count == 1:
+            mycursor = mydb.cursor()
+            query = "UPDATE users SET username = %s  WHERE username = %s"
+            value = (editUsername, username1)
+            mycursor.execute(query, value)
+            mydb.commit()
+            #self.editUsernameLabel.setText("")
+            self.saveChanges_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.profile_page))
+            self.displayProfile()
+        '''
+
+
+
+
+    def changePassword(self):
+        self.changePassCancel_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.profile_page))
+        username1 = self.userDisplayLabel.text()
+        oldPass = self.oldPass_lineEdit.text()
+        newPass = self.newPass_lineEdit.text()
+        count = 0
+
+        mydb = mc.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="detech"
+        )
+
+        if oldPass == "":
+            self.oldPassLabel.setText("Input password")
+        else:
+            self.oldPassLabel.setText("")
+            mycursor = mydb.cursor()
+            query = "SELECT * FROM users WHERE '" + oldPass + "' LIKE password AND '" + username1 + "' LIKE username"
+            mycursor.execute(query)
+            result = mycursor.fetchone()
+
+            if result is not None:
+                self.oldPassLabel.setText("")
+                count += 1
+            else:
+                self.oldPassLabel.setText("Incorrect old password")
+
+        if newPass == "":
+            self.oldPassLabel.setText("Input password")
+        else:
+            if len(newPass) < 8:
+                self.oldPassLabel.setText("New password must be at least 8!")
+            else:
+                if str.isalpha(newPass):
+                    self.oldPassLabel.setText("New password must have at least one integer!")
+                else:
+                    count += 1
+
+        if count == 2:
+            mycursor = mydb.cursor()
+            query = "UPDATE users SET password = %s , confirm_password = %s  WHERE username = %s"
+            value = (newPass, newPass, username1)
+            mycursor.execute(query, value)
+            mydb.commit()
+            self.oldPassLabel.setText("")
+            self.stackedWidget.setCurrentWidget(self.profile_page)
+            self.displayProfile()
+
+
+
+
+
+
+
+
 
 
 #--- LAUNCHING THE APPLICATION ---#
